cf413aaa5ad4c7d2c296b293eb2d9017c5932788
Roman Kopylov <romankopilov1712@gmail.com>
2023-04-07 13:43:39 +0300

    small changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,38 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const tabsContent = document.querySelectorAll('.tabcontent'),
-        tabs = document.querySelectorAll('.tabheader__item'),
-        tabsParent = document.querySelector('.tabheader__items');
 
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader__items'),
+        tabsContent = document.querySelectorAll('.tabcontent');
 
     function hideTabsContent() {
         tabsContent.forEach(tabContent => {
             tabContent.style.display = 'none';
-        })
+        });
         tabs.forEach(tab => {
-            tab.classList.remove('tabheader__item_active');
+            tab.classList.remove('tabheader__item_active')
         });
     }
-
     hideTabsContent();
 
     function showTabContent(tabNumber = 0) {
         tabsContent[tabNumber].style.display = 'block';
         tabs[tabNumber].classList.add('tabheader__item_active');
-
     }
 
-    showTabContent();
+    showTabContent()
 
     tabsParent.addEventListener('click', (event) => {
-
         const target = event.target;
+
         if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((tab, tabNumber) => {
                 if (target == tab) {
                     hideTabsContent();
-                    showTabContent(tabNumber);
+                    showTabContent(tabNumber)
                 }
             });
         }
     })
-
 });
